c10a4954158a04e4d85f87e1457abdb431e76eea
unknown <chungjimmy10@gmail.com>
2025-01-30 15:29:21 -0800

    added main function

--- DA/Dijkstra Algorithm.py
+++ DA/Dijkstra Algorithm.py
@@ -65,31 +65,35 @@ class Graph:
 				break
 		return '->'.join(path)
 
-g = Graph(7)
+def main():
+	g = Graph(7)
 
-g.add_node_data(0, 'A')
-g.add_node_data(1, 'B')
-g.add_node_data(2, 'C')
-g.add_node_data(3, 'D')
-g.add_node_data(4, 'E')
-g.add_node_data(5, 'F')
-g.add_node_data(6, 'G')
+	g.add_node_data(0, 'A')
+	g.add_node_data(1, 'B')
+	g.add_node_data(2, 'C')
+	g.add_node_data(3, 'D')
+	g.add_node_data(4, 'E')
+	g.add_node_data(5, 'F')
+	g.add_node_data(6, 'G')
 
-g.add_edge(3, 0, 4)  # D -> A, weight 5
-g.add_edge(3, 4, 2)  # D -> E, weight 2
-g.add_edge(0, 2, 3)  # A -> C, weight 3
-g.add_edge(0, 4, 4)  # A -> E, weight 4
-g.add_edge(4, 2, 4)  # E -> C, weight 4
-g.add_edge(4, 6, 5)  # E -> G, weight 5
-g.add_edge(2, 5, 5)  # C -> F, weight 5
-g.add_edge(1, 2, 2)  # B -> C, weight 2
-g.add_edge(1, 5, 2)  # B -> F, weight 2
-g.add_edge(6, 5, 5)  # G -> F, weight 5
+	g.add_edge(3, 0, 4)  # D -> A, weight 4
+	g.add_edge(3, 4, 2)  # D -> E, weight 2
+	g.add_edge(0, 2, 3)  # A -> C, weight 3
+	g.add_edge(0, 4, 4)  # A -> E, weight 4
+	g.add_edge(4, 2, 4)  # E -> C, weight 4
+	g.add_edge(4, 6, 5)  # E -> G, weight 5
+	g.add_edge(2, 5, 5)  # C -> F, weight 5
+	g.add_edge(1, 2, 2)  # B -> C, weight 2
+	g.add_edge(1, 5, 2)  # B -> F, weight 2
+	g.add_edge(6, 5, 5)  # G -> F, weight 5
 
-# Dijkstra's algorithm from D to all vertices
-print("Dijkstra's Algorithm starting from vertex D:\n")
-distance, predecessor = g.dijkstra('D')
-for i, d in enumerate(distance):
-	path= g.get_path(predecessor, 'D', g.nodes_list[i])
-	print(f"{path}, Distance: {d}")
+	# Dijkstra's algorithm from D to all vertices
+	print("Dijkstra's Algorithm starting from vertex D:\n")
+	distance, predecessor = g.dijkstra('D')
+	for i, d in enumerate(distance):
+		path= g.get_path(predecessor, 'D', g.nodes_list[i])
+		print(f"{path}, Distance: {d}")
+
+if __name__ == "__main__":
+    main()
 
